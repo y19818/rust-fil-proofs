3c0b6b2b21404ee75a0efdc3e8a1a45574ad7734
y19818 <31287334+y19818@users.noreply.github.com>
2021-01-29 15:00:04 +0800

    Update settings.rs

--- storage-proofs/core/src/settings.rs
+++ storage-proofs/core/src/settings.rs
@@ -47,7 +47,7 @@ impl Default for Settings {
             // `parameter_cache` does not use the cache() mechanism because it is now used
             // for durable, canonical Groth parameters and verifying keys.
             // The name is retained for backwards compatibility.
-            parameter_cache: "/var/tmp/filecoin-proof-parameters/".to_string(),
+            parameter_cache: "/data/lotus/folder/filecoin-proof-parameters/".to_string(),
             parent_cache: cache("filecoin-parents"),
             use_fil_blst: false,
         }
